fc9c9a849f7584cac1b012bd400126ebb7a1aebe
Charlieee_forfun <77525011+Charlieeecoding@users.noreply.github.com>
2024-04-06 18:25:39 +0800

    added one column - PLAYLIST_ID

--- Create_table_videos_information.sql
+++ Create_table_videos_information.sql
@@ -5,6 +5,7 @@ DROP TABLE YOUTUBE_API.DBO.VIDEOS_INFORMATION
 -- Step 2: Create a new table
 CREATE TABLE YOUTUBE_API.DBO.VIDEOS_INFORMATION (
   VIDEO_ID VARCHAR(MAX),
+  PLAYLIST_ID VARCHAR(MAX),
   TITLE VARCHAR(MAX),
   PUBLISH_DATE VARCHAR(MAX), 
   DESCRIPTION VARCHAR(MAX),
@@ -13,7 +14,7 @@ CREATE TABLE YOUTUBE_API.DBO.VIDEOS_INFORMATION (
 
 -- Step 3: Read data from the CSV file and insert into the table
 BULK INSERT YOUTUBE_API.DBO.VIDEOS_INFORMATION
-FROM 'videos_information.csv'
+FROM 'videos_information.csv' -- unable to share due to the large file size
 WITH (
   FIRSTROW = 2, -- Skip the header row if present
   FIELDTERMINATOR = ',', -- Specify the field delimiter
